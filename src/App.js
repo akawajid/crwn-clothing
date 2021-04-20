b54f74210daf7c2a681ad0a832c8c4f5e3b8553d
Wajid R <45237545+wajiddev@users.noreply.github.com>
2021-04-20 06:16:22 +0400

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const Hats = () => {
+  return <div>HatsPage</div>
+}
 
 function App() {
-  return <div><HomePage /></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
